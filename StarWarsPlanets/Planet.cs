712aafe4cadf28f43ea6f661f74b815f27ad11ad
Senior <Senior@G-02-06>
2024-09-09 12:53:56 -0400

    Add project files.

---
+++ StarWarsPlanets/Planet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarWarsPlanets
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public string Climate { get; set; }
+        public string Terrain { get; set; }
+        public int Population { get; set; }
+        public string[] Residents { get; set; }
+
+        public Planet(string name, string climate, string terrain, int population, string[] residents)
+        {
+            Name = name;
+            Climate = climate;
+            Terrain = terrain;
+            Population = population;
+            Residents = residents;
+        }
+
+        public void DisplayPlanetInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Climate: {Climate}");
+            Console.WriteLine($"Terrain: {Terrain}");
+            Console.WriteLine($"Population: {Population}");
+            Console.WriteLine("Residents:");
+            foreach (var resident in Residents)
+            {
+                Console.WriteLine($"- {resident}");
+            }
+            Console.WriteLine();
+        }
+
+        public static void AddPlanet(List<Planet> planets)
+        {
+            Console.Write("Enter the name of the planet: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the climate of the planet: ");
+            string climate = Console.ReadLine();
+
+            Console.Write("Enter the terrain of the planet: ");
+            string terrain = Console.ReadLine();
+
+            Console.Write("Enter the population of the planet: ");
+            int population = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of residents: ");
+            int numberOfResidents = int.Parse(Console.ReadLine());
+            string[] residents = new string[numberOfResidents];
+            for (int i = 0; i < numberOfResidents; i++)
+            {
+                Console.Write($"Enter resident {i + 1}: ");
+                residents[i] = Console.ReadLine();
+            }
+
+            planets.Add(new Planet(name, climate, terrain, population, residents));
+        }
+    }
+}
---
+++ StarWarsPlanets/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarWarsPlanets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Planet> planets = new List<Planet>();
+
+            planets.Add(new Planet("Tatooine", "Arid", "Desert", 200000, new string[] { "Luke Skywalker", "Anakin Skywalker" }));
+            planets.Add(new Planet("Alderaan", "Temperate", "Grasslands, Mountains", 1000000000, new string[] { "Leia Organa" }));
+            planets.Add(new Planet("Yavin IV", "Tropical", "Jungle", 30000, new string[] { "None" }));
+
+            Console.WriteLine("Star Wars Planets");
+            foreach (Planet planet in planets)
+            {
+                planet.DisplayPlanetInfo();
+            }
+
+            planets.Sort((p1, p2) => p1.Population.CompareTo(p2.Population));
+
+            Console.WriteLine("Planets sorted by population:");
+            foreach (Planet planet in planets)
+            {
+                planet.DisplayPlanetInfo();
+            }
+
+            bool addingPlanets = true;
+
+            while (addingPlanets)
+            {
+                Planet.AddPlanet(planets);
+
+                Console.Write("Do you want to add another planet? (y/n): ");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() != "y")
+                {
+                    addingPlanets = false;
+                }
+            }
+
+            planets.Sort((p1, p2) => p1.Population.CompareTo(p2.Population));
+
+            Console.WriteLine("\nUpdated list of planets:");
+            foreach (Planet planet in planets)
+            {
+                planet.DisplayPlanetInfo();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
